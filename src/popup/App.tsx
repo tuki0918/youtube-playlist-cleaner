bfb6437377a0d7a71511decd8a939863a9d47d94
Y.Yamamoto <tuki0918@gmail.com>
2024-09-09 23:55:39 +0900

    chore: format fix

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,28 +1,28 @@
 import { useCallback } from "react";
 
 function App() {
-	const handleClick = useCallback(async () => {
-		chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-			const tabId = tabs[0].id;
-			if (!tabId) {
-				console.log("No tabId");
-				return;
-			}
+  const handleClick = useCallback(async () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      const tabId = tabs[0].id;
+      if (!tabId) {
+        console.log("No tabId");
+        return;
+      }
 
-			await chrome.tabs.sendMessage(tabId, { tabId, action: "execute" });
-		});
-	}, []);
+      await chrome.tabs.sendMessage(tabId, { tabId, action: "execute" });
+    });
+  }, []);
 
-	return (
-		<>
-			<h1>YouTube Playlist Cleaner</h1>
-			<div>
-				<button type="button" onClick={handleClick}>
-					Clean!
-				</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <h1>YouTube Playlist Cleaner</h1>
+      <div>
+        <button type="button" onClick={handleClick}>
+          Clean!
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
