077b063c738cc4b0323cae6aec1c3d1155b008e3
Y.Yamamoto <tuki0918@gmail.com>
2024-09-11 01:44:51 +0900

    feat: Add loading state to the "Cleaning" button

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,7 +1,10 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 function App() {
+  // TODO: global state
+  const [isLoading, setIsLoading] = useState(false);
   const handleClick = useCallback(async () => {
+    setIsLoading(true);
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
       const tabId = tabs[0].id;
       if (!tabId) {
@@ -26,13 +29,26 @@ function App() {
 
           <div>
             <div className="m-4">
-              <button
-                type="button"
-                onClick={handleClick}
-                className="flex w-full h-12 items-center justify-center rounded-md bg-neutral-950 font-medium text-neutral-50 transition active:scale-110 hover:cursor-pointer"
-              >
-                Cleaning
-              </button>
+              {/* TODO: comporment */}
+              {isLoading ? (
+                <>
+                  <div className="flex w-full h-12 items-center justify-center rounded-md bg-neutral-950 font-medium text-neutral-50 space-x-3">
+                    <div className="h-2 w-2 bg-gray-200 rounded-full animate-ping" />
+                    <div className="h-2 w-2 bg-gray-200 rounded-full animate-ping [animation-delay:0.2s]" />
+                    <div className="h-2 w-2 bg-gray-200 rounded-full animate-ping [animation-delay:0.4s]" />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <button
+                    type="button"
+                    onClick={handleClick}
+                    className="flex w-full h-12 items-center justify-center rounded-md bg-neutral-950 font-medium text-neutral-50 transition active:scale-110 hover:cursor-pointer"
+                  >
+                    Cleaning
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
