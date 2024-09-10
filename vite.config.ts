3fa0eddf1fcf0dcbd1094fd71984576be0a0c989
Y.Yamamoto <tuki0918@gmail.com>
2024-09-10 20:32:26 +0900

    tailwindcss init

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { type PluginOption, defineConfig } from "vite";
 import manifest from "./manifest.config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [
+    react(),
+    crx({ manifest }),
+    // as PluginOption is a workaround for a bug in the tailwindcss plugin
+    tailwindcss() as unknown as PluginOption,
+  ],
 });
